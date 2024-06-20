daa281be6e9eb6943aef00c75047a3cc4810fc42
Dilip Mehata <d.mehata@gvc.co.jp>
2024-06-20 09:18:24 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+    # This method fetches the Gravatar image for the user
+   
+    def gravatar_for(user, size: 80 )
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # Hash the user's email
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id} ?s= #{ size }" # Create URL for Gravatar image
+      image_tag(gravatar_url, alt: user.name, class: "gravatar") # Return HTML tag for the image
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root  "static_pages#home" 
+  resources :users
+  root  "static_pages#home"
   # get "/help", to: "static_pages#help", as: 'helf'
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
