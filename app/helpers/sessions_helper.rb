a5e99ffa7ea90142fa92d6ba69e9689b60c4db5c
Dilip Mehata <d.mehata@gvc.co.jp>
2024-06-25 17:22:24 +0900

    Advaced-login is updated

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,7 @@ module SessionsHelper
   # Log in as the given user
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
   def remember(user)
@@ -10,17 +11,33 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+
   def current_user
-    if (user_id = session[:user_id])  # Check if there's a user ID in the session
-      @current_user ||= User.find_by(id: session[:user_id])  # Memoize the current user if not already set
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        log_in user  # Corrected 'log_in user = User' to 'log_in user'
-        @current_user = user  # Corrected '@currrent_user' to '@current_user'
+        log_in user
+        @current_user = user
       end
     end
   end
+
+  # def current_user
+  #   if (user_id = session[:user_id])  # Check if there's a user ID in the session
+  #     @current_user ||= User.find_by(id: session[:user_id])  # Memoize the current user if not already set
+  #   elsif (user_id = cookies.encrypted[:user_id])
+  #     user = User.find_by(id: user_id)
+  #     if user && user.authenticated?(cookies[:remember_token])
+  #       log_in user  # Corrected 'log_in user = User' to 'log_in user'
+  #       @current_user = user  # Corrected '@currrent_user' to '@current_user'
+  #     end
+  #   end
+  # end
 
   def logged_in?
     !current_user.nil?
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,90 +1,97 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
 
-
-  test  "login with invalid information "  do 
-    get  login_path 
-    assert_template  'sessions/new' 
-    post  login_path ,  params:  {  session:  {  email:  "" ,  password:  ""  }  } 
-    assert_response  :unprocessable_entity 
-    assert_template  'sessions/new' 
-    assert_not  flash.empty? 
-    get  root_path 
-    assert  flash.empty? 
-  end
-  
-  
   def setup
-    @user = users(:michael)  # Load test user
+    @user = users(:michael)
   end
+end
 
-  test "login with valid information" do
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
-    assert_redirected_to @user  # Check redirect to user's page
-    follow_redirect!  # Actually go to that page
-    assert_template 'users/show'  # Verify correct template is shown
-    assert_select "a[href=?]", login_path, count: 0  # Ensure "Log in" link is gone
-    assert_select "a[href=?]", logout_path  # Check "Log out" link is there
-    assert_select "a[href=?]", user_path(@user)  # Ensure profile link is shown
-  end
+class InvalidPasswordTest < UsersLogin
 
-
-  test "login with valid email/invalid password" do
+  test "login path" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: @user.email,  password: "invalid" } }
+  end
+
+  test "login with valid email/invalid password" do
+    post login_path, params: { session: { email:   @user.email, password: "invalid" } }
     assert_not is_logged_in?
-    assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
+end
 
-  test "login with valid information followed by logout" do
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
+class ValidLogin < UsersLogin
+
+  def setup
+    super
+    post login_path, params: { session: { email:    @user.email,  password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
     assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+
+  def setup
+    super 
     delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
-    delete logout_path
+  end
+
+  test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
-    # login with the valid information test is above where you can make it one by combination. alert
   end
 
   
 end
+
+class RememberLoginTest < UsersLogin 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    # assert_equal @user, assigns(:user)
+    # assert_equal cookies[:remember_token], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
+  end
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+    # assert_empty cookies[:remember_token].blank?
+  end
+
   
-# class LogoutTest < Logout
-#   test "should still work after logout in second window" do
-#     delete logout_path
-#     assert_redirected_to root_url
-#   end
-# end
-
-   
-class RememberingTest < UsersLoginTest
-  
-    test "login with remembeing" do
-      log_in_as(@user, remember_me: '1')
-      assert_not cookies[:remember_token].blank?
-    end
-
-    # test "login without remembering" do 
-    #   log_in_as(@user, remember_me: '1')
-    #   log_in_as(@user, remember_me: '0')    # here is the mistake please make sure to fix it and ask the yoya san
-    #   assert cookies[:remember_token].blank?
-    # end
-
-end
+end   
