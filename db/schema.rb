744b1018a8efe02f140bdf3a9654ee459ce699e3
Dilip Mehata <d.mehata@gvc.co.jp>
2024-06-26 17:29:47 +0900

    Database  Migration Updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2024_06_21_054314) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_032532) do
->>>>>>> updating-users
-=======
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_032532) do
->>>>>>> updating-users
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
